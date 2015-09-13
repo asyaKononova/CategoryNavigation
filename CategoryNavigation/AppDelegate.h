693a2f7fc135aaaeba2599d49a43fe4ebba9a125
Asya's Air <asya@Anastasias-MacBook-Air.local>
2015-09-13 12:40:43 +0300

    Initial Commit

---
+++ CategoryNavigation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CategoryNavigation
+//
+//  Created by Anastasia Kononova on 9/13/15.
+//  Copyright (c) 2015 Asya Kononova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CategoryNavigation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CategoryNavigation
+//
+//  Created by Anastasia Kononova on 9/13/15.
+//  Copyright (c) 2015 Asya Kononova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
